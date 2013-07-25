599cd969b824477ce32a62116de7493a237ba39a
Amy Erz <wingmerz@gmail.com>
2013-07-25 00:00:14 -0700

    add sorcery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tradr::Application.config.secret_token = '90aee6a32dd550143710001c8ca53ad2eb70af3e405a9cd8ba138645112b8e1719507bd3dd342700bd0444601d6a2f1dc9e16bcb42d16a31e80dc0118058cd2d'
